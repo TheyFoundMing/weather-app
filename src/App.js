9d533f32df769f95697fec4cb8d5cc26190a4599
Michaella Magtibay <michaeltmagtibay@gmail.com>
2020-10-12 19:34:31 +0700

    Add initial components, created Navbar

--- src/App.js
+++ src/App.js
@@ -1,26 +1,39 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import Navbar from "react-bootstrap/Navbar";
+
+import "./App.scss";
+
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      formCity: "",
+      formScale: 0,
+      fetchedData: {},
+    };
+  }
+
+  handleChange() {}
+  handleScale() {}
+
+  render() {
+    return (
+      <div>
+        <Navbar>
+          <Navbar.Brand>5 Days</Navbar.Brand>
+          <div>
+            <input type="text" placeholder="Search city" />
+            <button>search</button>
+            <a href="https://github.com/TheyFoundMing">Github</a>
+            <a href="https://www.linkedin.com/in/michaella-magtibay-7851421b2/">
+              LinkedIn
+            </a>
+          </div>
+        </Navbar>
+      </div>
+    );
+  }
 }
 
 export default App;
