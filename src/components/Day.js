d59129109004ff098dba1656a5713ddac3626c2c
Michaella Magtibay <michaeltmagtibay@gmail.com>
2020-10-19 20:13:57 +0700

    Add state and more elements

--- src/components/Day.js
+++ src/components/Day.js
@@ -1,12 +1,31 @@
 import React from "react";
 
-function Day(props) {
-  return (
-    <div>
-      This is a day
-      {/* {props["weather"]["0"]["description"]} */}
-    </div>
-  );
+import "../App.scss";
+
+class Day extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      formTime: 0,
+      times: this.props.data,
+      currentTime: this.props.data[0],
+    };
+  }
+
+  render() {
+    let currentDate = new Date(this.state.currentTime.dt_txt);
+    // console.log(currentDate.getDay());
+
+    return (
+      <div className="day">
+        <span>
+          {currentDate.getDate()}/{currentDate.getMonth()}/
+          {currentDate.getFullYear()}
+        </span>
+        {this.state.currentTime.weather[0]["description"]}
+      </div>
+    );
+  }
 }
 
 export default Day;
