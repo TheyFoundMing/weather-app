b879471977b06f809add1f0b8317be81ed12280f
Michaella Magtibay <michaeltmagtibay@gmail.com>
2020-10-24 19:00:05 +0700

    Add weather images, add more details in Day component

--- src/components/Day.js
+++ src/components/Day.js
@@ -2,6 +2,14 @@ import React from "react";
 
 import "../App.scss";
 
+import thunder from "../img/storm.png";
+import rainy from "../img/rainy.png";
+import drizzle from "../img/drizzle.png";
+import snow from "../img/storm.png";
+import atmosphere from "../img/haze.png";
+import clear from "../img/sun.png";
+import clouds from "../img/cloudy.png";
+
 class Day extends React.Component {
   constructor(props) {
     super(props);
@@ -14,15 +22,59 @@ class Day extends React.Component {
 
   render() {
     let currentDate = new Date(this.state.currentTime.dt_txt);
+    let weatherID = this.state.currentTime.weather[0]["id"];
+    let weatherImg;
     // console.log(currentDate.getDay());
+
+    switch (true) {
+      case weatherID >= 801:
+        weatherImg = clouds;
+        break;
+
+      case weatherID === 800:
+        weatherImg = clear;
+        break;
+
+      case weatherID >= 700:
+        weatherImg = atmosphere;
+        break;
+
+      case weatherID >= 600:
+        weatherImg = snow;
+        break;
+
+      case weatherID >= 500:
+        weatherImg = rainy;
+        break;
+
+      case weatherID >= 300:
+        weatherImg = drizzle;
+        break;
+
+      default:
+        weatherImg = thunder;
+        break;
+    }
 
     return (
       <div className="day">
-        <span>
-          {currentDate.getDate()}/{currentDate.getMonth()}/
-          {currentDate.getFullYear()}
-        </span>
-        {this.state.currentTime.weather[0]["description"]}
+        <div className="status">
+          <span>
+            {currentDate.getDate()}/{currentDate.getMonth()}/
+            {currentDate.getFullYear()}
+          </span>
+          {this.state.currentTime.weather[0]["description"]}
+        </div>
+
+        <img src={weatherImg} alt="" />
+
+        <div className="details">
+          <span>Temperature: {this.state.currentTime.main["temp"]}</span>
+          <span>Feels like: {this.state.currentTime.main["feels_like"]}</span>
+          <span>Max: {this.state.currentTime.main["temp_max"]}</span>
+          <span>Min: {this.state.currentTime.main["temp_min"]}</span>
+          <span>Humidity: {this.state.currentTime.main["humidity"]}</span>
+        </div>
       </div>
     );
   }
