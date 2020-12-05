854cd59f5d7175d0255efa5277cd1ea5441c3bb9
Michaella Magtibay <michaeltmagtibay@gmail.com>
2020-12-05 20:44:30 +0700

    Initial dist subtree commit

---
+++ build/precache-manifest.da1fac060eb1b037d51e30773fe10f7b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "00b47420f0f0760ac982a9a460daf334",
+    "url": "/weather-app/index.html"
+  },
+  {
+    "revision": "602084ef68763e496e6a",
+    "url": "/weather-app/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "24092bbe0a0958fc7cbb",
+    "url": "/weather-app/static/css/main.53f981b5.chunk.css"
+  },
+  {
+    "revision": "602084ef68763e496e6a",
+    "url": "/weather-app/static/js/2.4de7d059.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/weather-app/static/js/2.4de7d059.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "24092bbe0a0958fc7cbb",
+    "url": "/weather-app/static/js/main.f8fca661.chunk.js"
+  },
+  {
+    "revision": "d0e2e579b5155f255804",
+    "url": "/weather-app/static/js/runtime-main.e6f382d7.js"
+  }
+]);
